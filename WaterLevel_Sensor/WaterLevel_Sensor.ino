51da6a77f470f92f18baae5ef073debfaa2ae454
Angel Gerardo Jimenez Reyes <angel.g.j.reyes@outlook.com>
2021-11-27 19:57:26 -0600

    Rutina sensado de distancia Rutina de calculo de capacidad Rutina de envio de valor mejorada Rutina de Envio de estado de conexion Rutina de envio de conexion inicial

--- WaterLevel_Sensor/WaterLevel_Sensor.ino
+++ WaterLevel_Sensor/WaterLevel_Sensor.ino
@@ -10,15 +10,23 @@
 */
 //#define SERVER_IP "10.0.1.7:9080" // PC address with emulation on host
 #define SERVER_IP "192.168.31.65:25520"
+#define SERVER_IP_PROD "192.168.31.13:80"
 #define DEVICETYPE "water_level_sensor"
 #define DEVICENAME "Sensor_Nivel_de_Agua"
+#define DATATYPE "float"
+#define UNITVALUE "%"
+#define FIRMWAREVERSION "1.1.0"
 #ifndef STASSID
 #define STASSID "UBNT-HOME"
 #define STAPSK  "Brazil_2014"
 #endif
 
+#define _pintrig 0
+#define _pinecho 1
+
 char DEVICEID[] = "11AD6066-5088-47AE-B6B4-909AAB296958";
-// DEVICEID = "11AD6066-5088-47AE-B6B4-909AAB296958";
+
+int profundidad = 126;
 
 void setup() {
 
@@ -27,6 +35,9 @@ void setup() {
   Serial.println();
   Serial.println();
   Serial.println();
+
+  pinMode(_pinecho, INPUT);
+  pinMode(_pintrig, OUTPUT);
 
   WiFi.hostname(DEVICENAME);
   WiFi.begin(STASSID, STAPSK);
@@ -40,33 +51,42 @@ void setup() {
   Serial.println(WiFi.localIP());
   delay(500);
 
-  SendConnection();
+  SendConnection("http://" SERVER_IP "/api/IOTDevice/Add");
+  SendConnection("http://" SERVER_IP_PROD "/api/IOTDevice/Add");
 }
 
 void loop() {
-  SendValue();
-  UpdateConnectionStatus();
+  //SendValue("http://" SERVER_IP "/api/Data/Add", GetActualCapacity(GetDistance()));
+  SendValue("http://" SERVER_IP_PROD "/api/Data/Add", GetActualCapacity(GetDistance()));
+
+  SendValue("http://" SERVER_IP "/api/Data/Add", GetDistance());
+  //SendValue("http://" SERVER_IP_PROD "/api/Data/Add", GetDistance());
+
+  UpdateConnectionStatus("http://" SERVER_IP "/api/IOTDevice/Update/" + String(DEVICEID));
+  UpdateConnectionStatus("http://" SERVER_IP_PROD "/api/IOTDevice/Update/" + String(DEVICEID));
 
   delay(60000);
+  //delay(10000);
 }
 
-void SendValue() {
+void SendValue(String server, float value) {
   // wait for WiFi connection
   if ((WiFi.status() == WL_CONNECTED)) {
 
     WiFiClient client;
     HTTPClient http;
+    String data = "{\"IDDevice\":\""+ String(DEVICEID) +"\",\"value\":\"" + String(value, 2) +"\",\"DataType\": \"float\"}";
 
     Serial.print("[HTTP] begin...\n");
     // configure traged server and url
-    http.begin(client, "http://" SERVER_IP "/api/Data/Add"); //HTTP
+    http.begin(client, server); //HTTP
     http.addHeader("Content-Type", "application/json");
 
     Serial.print("[HTTP] POST...\n");
-    float value = getRandom();
-    Serial.println(WiFi.localIP().toString() +">>: {\"IDDevice\":\""+ String(DEVICEID) +"\",\"value\":\"" + String(value, 2) +"\",\"DataType\": \"float\"}");
+    // float value = getRandom();
+    Serial.println(WiFi.localIP().toString() +">>: " + data);
     // start connection and send HTTP header and body
-    int httpCode = http.POST("{\"IDDevice\":\""+ String(DEVICEID) +"\",\"value\":\"" + String(value, 2) +"\",\"DataType\": \"float\"}");
+    int httpCode = http.POST(data);
 
     // httpCode will be negative on error
     if (httpCode > 0) {
@@ -88,7 +108,7 @@ void SendValue() {
   }
 }
 
-void UpdateConnectionStatus() {
+void UpdateConnectionStatus(String server) {
   if ((WiFi.status() == WL_CONNECTED)) {
     WiFiClient client;
     HTTPClient http;
@@ -96,7 +116,7 @@ void UpdateConnectionStatus() {
 
     Serial.print("[HTTP] begin...\n");
     // configure traged server and url
-    http.begin(client, "http://" SERVER_IP "/api/IOTDevice/Update/" + String(DEVICEID)); //HTTP
+    http.begin(client, server); //HTTP
     http.addHeader("Content-Type", "application/json");
 
     Serial.print("[HTTP] POST...\n");
@@ -124,15 +144,15 @@ void UpdateConnectionStatus() {
   }
 }
 
-void SendConnection() {
+void SendConnection(String server) {
   if ((WiFi.status() == WL_CONNECTED)) {
     WiFiClient client;
     HTTPClient http;
-    String data = "{\"IDDevice\":\""+ String(DEVICEID) +"\", \"MacAddress\":\""+ WiFi.macAddress() +"\", \"IPAddress\":\""+ WiFi.localIP().toString() +"\", \"DeviceType\":\""+ DEVICETYPE +"\", \"DeviceName\":\""+ DEVICENAME +"\", \"DataType\":\"float\", \"MeasurementUnit\":\"%\", \"FirmwareVersion\":\"1.0.1\", \"DeviceStatus\":\"Online\"}";
+    String data = "{\"IDDevice\":\""+ String(DEVICEID) +"\", \"MacAddress\":\""+ WiFi.macAddress() +"\", \"IPAddress\":\""+ WiFi.localIP().toString() +"\", \"DeviceType\":\""+ DEVICETYPE +"\", \"DeviceName\":\""+ DEVICENAME +"\", \"DataType\":\""+ DATATYPE +"\", \"MeasurementUnit\":\""+ UNITVALUE +"\", \"FirmwareVersion\":\""+ FIRMWAREVERSION +"\", \"DeviceStatus\":\"Online\"}";
 
     Serial.print("[HTTP] begin...\n");
     // configure traged server and url
-    http.begin(client, "http://" SERVER_IP "/api/IOTDevice/Add"); //HTTP
+    http.begin(client, server); //HTTP
     http.addHeader("Content-Type", "application/json");
 
     Serial.print("[HTTP] POST...\n");
@@ -158,6 +178,31 @@ void SendConnection() {
 
     http.end();
   }
+}
+
+float GetDistance() { // Centimeters
+  // Clears the trigPin condition
+  digitalWrite(_pintrig, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+  digitalWrite(_pintrig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(_pintrig, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  long duration = pulseIn(_pinecho, HIGH);
+  // Calculating the distance
+  return duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+}
+
+float GetActualCapacity(float distance) {
+  if (distance > profundidad)
+  {
+    return 0;
+  } else if (distance < 0) {
+    return 100;
+  } else {
+    return ((distance - profundidad) * 100 / profundidad) * (-1);
+  }    
 }
 
 float getRandom() {
