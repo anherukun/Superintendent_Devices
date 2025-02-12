c14029a3dd4a10f93ba55e562be80a1ad414de3f
Angel Gerardo Jimenez Reyes <angel.g.j.reyes@gmail.com>
2025-02-12 13:45:13 -0600

    metodo 2 para leer bytes de entrada

--- Ecozy_Ambi-Lights/Ecozy_Ambi-Lights.ino
+++ Ecozy_Ambi-Lights/Ecozy_Ambi-Lights.ino
@@ -1,11 +1,18 @@
 #include <Adafruit_NeoPixel.h>
 
-#define RX_BUF_SIZE 1024
+#define RX_BUF_SIZE 2048
 
 byte pixelPin = 4;
 byte pixelCount = 128;
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(pixelCount, pixelPin, NEO_GRB + NEO_KHZ800);
+
+byte pCounter = 0;
+byte subCounter = -1;
+
+byte rColor = 0;
+byte gColor = 0;
+byte bColor = 0;
 
 void setup()
 {
@@ -13,14 +20,15 @@ void setup()
     Serial.begin(9600);
     pixels.begin();
 
-    // pixels.setBrightness(255 / 2);
+    pixels.setBrightness((255 / 3) * 2);
 }
 
 // the loop function runs over and over again forever
 void loop()
 {
-    method_1();
-    delay(1);
+    // method_1();
+    method_2();
+    // delay(1);
     Serial.flush();
 }
 
@@ -29,7 +37,7 @@ void method_1()
     if (Serial.available())
     {
         // inByte = Serial.read();
-        int i_code = Serial.read();
+        byte i_code = Serial.read();
         byte r_code = Serial.read();
         byte g_code = Serial.read();
         byte b_code = Serial.read();
@@ -37,4 +45,36 @@ void method_1()
         pixels.setPixelColor(i_code, r_code, g_code, b_code);
         pixels.show();
     }
+}
+
+void method_2()
+{
+    if (Serial.available())
+    {
+        subCounter++;
+
+        switch (subCounter)
+        {
+        case 0:
+            rColor = Serial.read();
+            break;
+        case 1:
+            gColor = Serial.read();
+            break;
+        case 2:
+            bColor = Serial.read();
+            break;
+        case 3:
+            pixels.setPixelColor(pCounter, rColor, gColor, bColor);
+            pCounter += 1;
+            subCounter = -1;
+
+            if (pCounter >= pixelCount) {
+                pixels.show();
+                pCounter = 0;
+            }
+            break;
+        }
+    }
+
 }
